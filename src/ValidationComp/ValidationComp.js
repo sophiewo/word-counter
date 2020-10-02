461434a0cb1655a0210552103b4c76c1e9563e42
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-10-02 18:04:50 +0100

    Amends component formatting

--- src/ValidationComp/ValidationComp.js
+++ src/ValidationComp/ValidationComp.js
@@ -5,7 +5,7 @@ import React from 'react';
 //Inside theValidationComponent, either output "Text too short" or "Text long enough" depending on the text length(e.g.take 5 as a minimum length)
 
 const validationComponent = (prop) => {
-  let textReturn = "Bang on point";
+  let textReturn = "bang on point";
   if (prop.textLength < 5) {
     textReturn = "too short"  
   } else if (prop.textLength > 20) {
@@ -14,8 +14,7 @@ const validationComponent = (prop) => {
 
   return (
     <div className="ValidationComp">
-      <p> The length of your text is: {prop.textLength} characters</p>
-      <p> Text length: {textReturn} </p>
+      <p> The length of your text is {prop.textLength} characters which is {textReturn} </p>
     </div>
   )
 }
